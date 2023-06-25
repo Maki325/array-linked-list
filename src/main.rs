0d1b2b5a85e7c6315c04d7cb5d3daa18ffc36443
Marko <markoni521@gmail.com>
2023-06-25 03:33:38 +0200

    Added tests. Made List and Node clonable

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
   ops::{Index, IndexMut},
 };
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 #[repr(C)]
 struct Node<T> {
   data: T,
@@ -13,7 +13,7 @@ struct Node<T> {
   position_offset: isize,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct List<T> {
   head: Option<usize>,
   tail: Option<usize>,
@@ -145,6 +145,14 @@ impl<T> List<T> {
     }
   }
 
+  pub fn len(&self) -> usize {
+    return self.nodes.len();
+  }
+
+  pub fn capacity(&self) -> usize {
+    return self.nodes.capacity();
+  }
+
   pub fn into_iter(self) -> ListIter<T> {
     return ListIter::new(self.nodes);
   }
@@ -180,13 +188,14 @@ impl<T> Index<usize> for List<T> {
   type Output = T;
 
   fn index(&self, idx: usize) -> &Self::Output {
-    return &self.nodes[idx as usize].data;
+    return &self.nodes[self.get_actual_index(idx)].data;
   }
 }
 
 impl<T> IndexMut<usize> for List<T> {
   fn index_mut(&mut self, idx: usize) -> &mut Self::Output {
-    return &mut self.nodes[idx as usize].data;
+    let idx = self.get_actual_index(idx);
+    return &mut self.nodes[idx].data;
   }
 }
 
@@ -273,6 +282,187 @@ impl_ref_iter! {
   'a,
   &;
   mut
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn new() {
+    let list = List::<usize>::new();
+    assert_eq!(list.len(), 0);
+  }
+
+  #[test]
+  fn with_capacity() {
+    const CAP: usize = 10;
+
+    let list = List::<usize>::with_capacity(CAP);
+    assert_eq!(list.capacity(), CAP);
+  }
+
+  #[test]
+  fn push() {
+    let mut list = List::<usize>::new();
+
+    list.push(25);
+
+    assert_eq!(list[0], 25);
+  }
+
+  #[test]
+  fn push_multiple() {
+    let mut list = List::<usize>::new();
+
+    list.push(25);
+    list.push(50);
+    list.push(75);
+    list.push(100);
+
+    assert_eq!(list[0], 25);
+    assert_eq!(list[1], 50);
+    assert_eq!(list[2], 75);
+    assert_eq!(list[3], 100);
+  }
+
+  #[test]
+  fn insert_start() {
+    let mut list = List::<usize>::new();
+
+    list.push(25);
+    list.insert(0, 20);
+
+    assert_eq!(list[0], 20);
+    assert_eq!(list[1], 25);
+  }
+
+  #[test]
+  fn insert_multiple_start() {
+    let mut list = List::<usize>::new();
+
+    list.push(25);
+    list.insert(0, 20);
+    list.insert(0, 15);
+    list.insert(0, 10);
+
+    assert_eq!(list[0], 10);
+    assert_eq!(list[1], 15);
+    assert_eq!(list[2], 20);
+    assert_eq!(list[3], 25);
+  }
+
+  #[test]
+  fn insert_end() {
+    let mut list = List::<usize>::new();
+
+    list.push(25);
+    list.insert(1, 50);
+
+    assert_eq!(list[0], 25);
+    assert_eq!(list[1], 50);
+  }
+
+  #[test]
+  fn insert_multiple_end() {
+    let mut list = List::<usize>::new();
+
+    list.push(25);
+    list.insert(1, 50);
+    list.insert(2, 75);
+    list.insert(3, 100);
+
+    assert_eq!(list[0], 25);
+    assert_eq!(list[1], 50);
+    assert_eq!(list[2], 75);
+    assert_eq!(list[3], 100);
+  }
+
+  #[test]
+  fn insert() {
+    let mut list = List::<usize>::new();
+
+    list.push(25);
+    list.push(75);
+    list.insert(1, 50);
+
+    assert_eq!(list[0], 25);
+    assert_eq!(list[1], 50);
+    assert_eq!(list[2], 75);
+  }
+
+  #[test]
+  fn insert_multiple() {
+    let mut list = List::<usize>::new();
+
+    list.push(25);
+    list.push(75);
+    list.insert(1, 50);
+    list.insert(1, 45);
+    list.insert(1, 40);
+
+    assert_eq!(list[0], 25);
+    assert_eq!(list[1], 40);
+    assert_eq!(list[2], 45);
+    assert_eq!(list[3], 50);
+    assert_eq!(list[4], 75);
+  }
+
+  #[test]
+  #[should_panic]
+  fn insert_out_of_bounds() {
+    let mut list = List::<usize>::new();
+
+    list.push(25);
+    list.push(75);
+    list.insert(25, 50);
+  }
+
+  #[test]
+  fn into_iter_use_after() {
+    let mut list = List::<usize>::new();
+
+    list.push(25);
+    list.push(50);
+    list.push(75);
+
+    for (idx, value) in list.clone().into_iter().enumerate() {
+      assert_eq!(list[idx], value);
+    }
+  }
+
+  #[test]
+  fn iter() {
+    let mut list = List::<usize>::new();
+
+    list.push(25);
+    list.push(50);
+    list.push(75);
+
+    for (idx, value) in list.iter().enumerate() {
+      assert_eq!(&list[idx], value);
+    }
+  }
+
+  #[test]
+  fn iter_mut() {
+    let mut list = List::<usize>::new();
+
+    list.push(25);
+    list.push(50);
+    list.push(75);
+
+    let old = list.clone();
+
+    for value in &mut list {
+      *value += 100;
+    }
+
+    for (idx, value) in list.iter().enumerate() {
+      assert_eq!(&list[idx], value);
+      assert_eq!(&(old[idx] + 100), value);
+    }
+  }
 }
 
 fn main() {
